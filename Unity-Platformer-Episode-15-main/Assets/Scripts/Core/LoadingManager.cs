924e61f547a2717d8d50f00b6c95d3fc9010dcad
Jeralith <tuananh2634@gmail.com>
2025-06-29 22:47:06 +0700

    add menu, select lvl

--- Unity-Platformer-Episode-15-main/Assets/Scripts/Core/LoadingManager.cs
+++ Unity-Platformer-Episode-15-main/Assets/Scripts/Core/LoadingManager.cs
@@ -5,9 +5,11 @@ public class LoadingManager : MonoBehaviour
 {
     public static LoadingManager instance { get; private set; }
     public string levelName;
-
+    public GameObject mainMenu;
+    public GameObject levelSelectUI;
     private void Awake()
     {
+        OpenMainMenu();
         //Keep this object even when we go to new scene
         if (instance == null)
         {
@@ -30,10 +32,36 @@ public class LoadingManager : MonoBehaviour
     }
     public static void LoadLevel1()
     {
+        Time.timeScale = 1;
         SceneManager.LoadScene("Level1");
     }
     public static void LoadLevel2()
     {
+        Time.timeScale = 1;
         SceneManager.LoadScene("Level2");
+    }
+
+    public void OpenLevelSelect()
+    {
+        if (levelSelectUI != null && mainMenu != null)
+        {
+            levelSelectUI.SetActive(true);
+            mainMenu.SetActive(false);
+        }
+
+    }
+    public void OpenMainMenu()
+    {
+        Time.timeScale = 1;
+        if (levelSelectUI != null && mainMenu != null)
+        {
+            levelSelectUI.SetActive(false);
+            mainMenu.SetActive(true);
+        }
+    }
+
+    public void Quit()
+    {
+        Application.Quit();
     }
 }
